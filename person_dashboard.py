58f785ce1cecdf6266aec4add8573ad4f82d4f21
Bryan Owens <bryan.owens@yale.edu>
2021-01-05 19:19:30 -0800

    Complete most of part 4: random users and login screen

--- person_dashboard.py
+++ person_dashboard.py
@@ -1,6 +1,7 @@
 import requests
 import streamlit as st
 from dashboard import get_response
+import hashlib
 
 RANDOMUSER_API_BASE = 'https://randomuser.me/api'
 
@@ -10,6 +11,7 @@ class User:
         self.last_name = response['name']['last']
         self.dob = response['dob']['date']
         self.username = response['login']['username']
+        self.salt = response['login']['salt']
         self.hashed_password = response['login']['sha256']
 
 def generate_users(n_users):
@@ -21,15 +23,34 @@ def generate_users(n_users):
     for user_response in response['results']:
         user = User(user_response)
         username = user.username
-        users[username] = User
+        users[username] = user
 
     return users
 
+def validate_user(users, username, password):
+    hashed_password = users[username].hashed_password
+    return hashlib.sha256(password) == hashed_password
+
 def main():
     N_USERS = 100
-
     users = generate_users(N_USERS)
-    print(users)
+
+    # For debugging:
+    print([{username: f'{user.first_name} {user.last_name}'} for username, user in users.items()])
+
+    # Login form
+    # https://discuss.streamlit.io/t/is-there-a-way-to-create-a-form-with-streamlit/7057/2
+    st.markdown('Please login:')
+    username = st.text_input('Username')
+    password = st.text_input('Password', type = 'password')
+    user = None
+
+    if username in users:
+        user = users[username]
+
+    if user and validate_user(users, username, password + user.salt):
+        st.markdown(f'Welcome back, {user.first_name} {user.last_name}! Your DOB is {user.dob}')
+
 
 if __name__ == '__main__':
     main()
