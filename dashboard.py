55068f4a34f703c1e1ee05137e32e378234b4447
kylejb <47502769+kylejb@users.noreply.github.com>
2021-03-24 17:40:06 -0400

    Reformatted with black and flake8

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,13 @@
-"""Main module for the Streamlit app"""
+"""Main module for the Streamlit app."""
 import requests
 import streamlit as st
 
-NAGER_API_BASE = 'https://date.nager.at/api/v2'
+NAGER_API_BASE = "https://date.nager.at/api/v2"
 
 
 @st.cache
 def load_country_codes():
-    """Loads country codes available from the Nager.Date API
+    """Load country codes available from the Nager.Date API.
 
     Returns:
         A list of country codes. Each country code is
@@ -17,16 +17,14 @@ def load_country_codes():
         requests.exceptions.RequestException: If the
             request to the Nager.Date API fails.
     """
-
-
-    url = '/'.join([NAGER_API_BASE, 'AvailableCountries'])
+    url = "/".join([NAGER_API_BASE, "AvailableCountries"])
     try:
         response = requests.get(url)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-            raise SystemExit(e)
+        raise SystemExit(e)
 
-    #### TODO - Process the response ####
+    # TODO - Process the response ####
 
     country_codes = response.json()
 
@@ -36,16 +34,14 @@ def load_country_codes():
 
 
 def main():
-    st.markdown('Have fun!')
+    st.markdown("Have fun!")
 
     country_codes = load_country_codes()
 
-    country_code = st.selectbox('Select a country code',
-                                country_codes)
+    country_code = st.selectbox("Select a country code", country_codes)
 
-    st.markdown('You selected country code -', country_code)
+    st.markdown("You selected country code -", country_code)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
